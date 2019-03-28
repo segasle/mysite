871087777deb743620a253906d79bfbc8881f63c
sergej <segasle@gmail.com>
2019-03-28 17:33:55 +0300

    структура внешшней вида формы

--- inip/comments.php
+++ inip/comments.php
@@ -1,7 +1,18 @@
+<h1>Отзывы</h1>
 <?php
-/**
- * Created by PhpStorm.
- * User: sergej
- * Date: 28.03.19
- * Time: 16:57
- */
+if (isset($_SESSION['data'])) {?>
+    <form action="" method="post">
+        <div class="form_grog">
+            <p>Сообщения</p>
+            <textarea class="form-control" name="text" placeholder="Сообщение"><?php echo @$_POST['text']; ?></textarea>
+        </div>
+        <div class="form_grog">
+            <button type="submit" class="btn">Отправить</button>
+        </div>
+    </form>
+<?php
+}else{?>
+    <p class="h3">Авторизовуйтесь, чтобы написать отзыв</p>
+<?php
+}
+?>

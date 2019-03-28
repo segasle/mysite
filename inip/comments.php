63e71d9d7983d553b805fc6e1262de2f53be3ae1
segasle <segasle@gmail.com>
2019-03-28 18:27:24 +0300

    оотзыв

--- inip/comments.php
+++ inip/comments.php
@@ -7,12 +7,25 @@ if (isset($_SESSION['data'])) {?>
             <textarea class="form-control" name="text" placeholder="Сообщение"><?php echo @$_POST['text']; ?></textarea>
         </div>
         <div class="form_grog">
-            <button type="submit" class="btn">Отправить</button>
+            <button type="submit" class="btn" name="submit">Отправить</button>
         </div>
     </form>
 <?php
+    if (isset($_POST['submit'])){
+        $data = $_POST;
+        $errors = array();
+        if (empty($data['text'])){
+            $errors[] = 'Сообщение пустое';
+        }
+        if (empty($errors)){
+
+        }else{
+            echo '<div class="errors">'.array_shift($errors).'</div>';
+        }
+    }
 }else{?>
     <p class="h3">Авторизовуйтесь, чтобы написать отзыв</p>
 <?php
 }
+
 ?>

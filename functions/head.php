669248eea2a4ba8d02045853bc70c4a7cdc239db
sergej <segasle@gmail.com>
2019-03-29 11:54:42 +0300

    head title

--- functions/head.php
+++ functions/head.php
@@ -1,5 +1,12 @@
 <?php
 global $title;
+$page = basename($_SERVER['REQUEST_URI']);
+$res = do_query("SELECT * FROM `meta_title` JOIN `menu` ON meta_title.url = menu.url WHERE meta_title.url = '{$page}'");
+foreach ($res as $item) {
+    if (basename($_SERVER['REQUEST_URI']) == $item['url']) {
+        $title = $item['title'];
+    }
+}
 if (basename($_SERVER['REQUEST_URI']) == '?page=main') {
     $title = 'Главная страница о Сергее Слепенкова';
 }
@@ -9,12 +16,16 @@ if (basename($_SERVER['REQUEST_URI']) == '') {
 
 if (basename($_SERVER['REQUEST_URI']) == '?page=experience') {
     $title = 'Навыки';
-}if (basename($_SERVER['REQUEST_URI']) == '?page=blog') {
+}
+if (basename($_SERVER['REQUEST_URI']) == '?page=blog') {
     $title = 'блог';
-}if (basename($_SERVER['REQUEST_URI']) == '?page=project') {
+}
+if (basename($_SERVER['REQUEST_URI']) == '?page=project') {
     $title = 'Проекты';
-}if (basename($_SERVER['REQUEST_URI']) == '?page=contacts') {
+}
+if (basename($_SERVER['REQUEST_URI']) == '?page=contacts') {
     $title = 'Контакты';
-}if (basename($_SERVER['REQUEST_URI']) == '?page=design') {
+}
+if (basename($_SERVER['REQUEST_URI']) == '?page=design') {
     $title = 'Работы по графическому дизайну';
 }

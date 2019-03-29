6ddf7ba68e9b7ce0ca8ed79ae40a3736929f245d
segasle <segasle@gmail.com>
2019-03-29 21:35:18 +0300

    структура шаблона заказа

--- inip/products.php
+++ inip/products.php
@@ -1,7 +1,32 @@
+<h1>Заказ проектов</h1>
 <?php
-/**
- * Created by PhpStorm.
- * User: sergej
- * Date: 29.03.19
- * Time: 16:48
- */
+$res = do_query("SELECT * FROM `products`");
+$out = "<div class='container'><div class='row'>";
+foreach ($res as $item){
+    $out .= "<div class='col-lg-4 col-xs-12'>
+                <div class='container_block'>
+                    <div class='container_block-head'>
+                        <p class='h3 text-center'>".$item['name']."</p>
+                    </div>
+                    <div class='container_block-content'>
+                    
+                    </div>
+                    <div class='container_block-footer'>
+                        <div class='footer_content'>
+                            <div class='footer_content-text'>
+                                <p class='h4 text-center'>От ".$item['price']."</p>
+                            </div> 
+                            <div class='footer_content-form'>
+                                <form action=''>
+                                    <div class='form-group'>
+                                        <button type='submit' name='".$item['id']."'>Закать</button>
+                                    </div>
+                                </form>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+            </div>";
+}
+$out .= '</div></div>';
+echo $out;

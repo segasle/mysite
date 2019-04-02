c12386f67f12a4475ff1d54f33b127f2b70629b9
sergej <segasle@gmail.com>
2019-04-02 13:28:31 +0300

    поменял структуру фудера

--- tempate/footer.php
+++ tempate/footer.php
@@ -1,26 +1,37 @@
-
 </div>
 </description>
 <desctiption>
     <div class="cellar">
-<div class="">
-    <p>Все права защищены @<?php echo date('Y');?></p>
-</div>
-        <div class="block_icon">
-            <div class="img_icon">
-                <a href="https://ok.ru/profile/522349619246" class="icon1 img_icons"></a>
-                <a href="https://twitter.com/segasle?lang=ru" class="icon2 img_icons"></a>
-                <a href="https://www.instagram.com/segasle1998/?hl=ru" class="icon3 img_icons"></a>
-                <a href="https://www.facebook.com/segasle" class="icon4 img_icons"></a>
-                <a href="skype: slepenkovs" class="icon5 img_icons"></a>
-                <a href="https://vk.com/serg_slepenkov" class="icon6 img_icons"></a>
+        <div class="row">
+            <div class="col-lg-3 col-xs-12">
+                <div class="">
+                    <p>Все права защищены &copy;<?php echo date('Y'); ?></p>
+                </div>
+            </div>
+            <div class="col-lg-9 col-xs-12">
+                <div class="block_icon">
+                    <div class="img_icon">
+                        <a href="https://ok.ru/profile/522349619246" class="icon1 img_icons"></a>
+                        <a href="https://twitter.com/segasle?lang=ru" class="icon2 img_icons"></a>
+                        <a href="https://www.instagram.com/segasle1998/?hl=ru" class="icon3 img_icons"></a>
+                        <a href="https://www.facebook.com/segasle" class="icon4 img_icons"></a>
+                        <a href="skype: slepenkovs" class="icon5 img_icons"></a>
+                        <a href="https://vk.com/serg_slepenkov" class="icon6 img_icons"></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </desctiption>
 </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>

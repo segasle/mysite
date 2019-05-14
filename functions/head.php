3d90b33e18a1d03cff9b7a3e914f16fd32975f9e
segasle <segasle@gmail.com>
2019-05-14 20:27:03 +0300

    Оптитация мета

--- functions/head.php
+++ functions/head.php
@@ -1,9 +1,13 @@
 <?php
 global $title;
+global $keywords;
+global $description;
 $page = basename($_SERVER['REQUEST_URI']);
 $res = do_query("SELECT * FROM `meta_title` JOIN `menu` ON meta_title.url_meta = menu.url WHERE meta_title.url_meta = '{$page}'");
 foreach ($res as $item) {
     if ($page == $item['url_meta']) {
         $title = $item['title_meta'] .' | Сергей Слепенков - ssd18.ru';
+        $keywords = $item['keywords_meta'];
+        $description = $item['description_meta'];
     }
 }
--- tempate/header.php
+++ tempate/header.php
@@ -9,6 +9,8 @@ if (isset($_SESSION['data'])) {
 <!doctype html>
 <html lang="ru">
 <head>
+    <?php include 'functions/head.php'; ?>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -16,14 +18,13 @@ if (isset($_SESSION['data'])) {
     <meta name="yandex-verification" content="1828ff9bd0a32839"/>
     <meta property="og:locale" content="ru_RU">
     <meta property="og:type" content="article">
-    <meta property="og:title" content="Сергей Слепенков - front-end разработчик">
+    <meta property="og:title" content="Сергей Слепенков - back-end разработчик">
     <meta property="og:description" content="ru_RU">
     <meta property="og:site_name" content="">
     <meta name="robots" content="index, follow">
     <meta name="keywords"
-          content="Сергей Слепенков, front-end разработчик, верстальщик, web-программист, web-дизайнер, программист">
-    <meta name="description" content="">
-    <?php include 'functions/head.php'; ?>
+          content="<?php echo $keywords; ?>">
+    <meta name="description" content="<?php echo $description; ?>">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="icons/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"

7f81156d324653b182ffc5bc47d28a96cf3d4cf5
segasle <segasle@gmail.com>
2019-05-14 14:51:00 +0300

    добавил классы

--- inip/products.php
+++ inip/products.php
@@ -7,9 +7,9 @@ $out = "<div class='container'><div class='row'>";
 foreach ($res as $item) {
     $id = $item['id_products'];
     $des = '';
-    foreach ($query as $items){
-        if ($id == $items['id_id_products']){
-            $des .=  "<li class='fa fa-check-square-o fa-2x'><p>".$items['description']."</p></li>";
+    foreach ($query as $items) {
+        if ($id == $items['id_id_products']) {
+            $des .= "<li class='float-left fa fa-check-square-o fa-2x'><div class='float-right'><p>" . $items['description'] . "</p></div></li>";
 
         }
     }
@@ -20,7 +20,7 @@ foreach ($res as $item) {
                     </div>
                     <div class='container_block-content'>
                     <ul>
-                  ".$des."
+                  " . $des . "
 </ul>
                     </div>
                     <div class='container_block-footer'>
@@ -44,32 +44,32 @@ foreach ($res as $item) {
             echo '<p class="h3 text-center">Авторизовуйтесь пожалуйста, чтобы заказать сайт</p>';
         } else {
             $data = do_query("SELECT * FROM `users` JOIN `products` WHERE users.email = '{$_SESSION['data']['email']}' AND products.id_products = $id ");
-            if ($data){
+            if ($data) {
                 $_SESSION['prod'] = $data;
 
-                if (isset($_SESSION['prod'])){
+                if (isset($_SESSION['prod'])) {
 //
 //                    echo '<pre>';
 //                    print_r($_SESSION['prod']);
 //                    echo '</pre>';
-                    $fio ='';
-                    $email ='';
-                    $order ='';
-                    foreach ($_SESSION['prod'] as $value){
-                        $fio = 'Имя и фамилия: ' .$value['name'] .' '.$value['surname'];
-                        $email = 'Почта: ' .$value['email'];
-                        $order = 'Заказ: '.$value['name_products'];
+                    $fio = '';
+                    $email = '';
+                    $order = '';
+                    foreach ($_SESSION['prod'] as $value) {
+                        $fio = 'Имя и фамилия: ' . $value['name'] . ' ' . $value['surname'];
+                        $email = 'Почта: ' . $value['email'];
+                        $order = 'Заказ: ' . $value['name_products'];
                         //$sms = 'Сообщение: ' .$data['text'];
                     }
 
-                    $mess = $fio . '<br>'.$email.'<br>'.$order.'<br>';
-                    $to      = 'segasle@yandex.ru';
+                    $mess = $fio . '<br>' . $email . '<br>' . $order . '<br>';
+                    $to = 'segasle@yandex.ru';
                     $subject = 'Заказ';
                     $message = "$mess";
                     $headers = 'From: segasle@kafe-lyi.ru' . "\r\n" .
                         'Reply-To: segasle@kafe-lyi.ru' . "\r\n" .
                         "Content-Type: text/html; charset=\"UTF-8\"\r\n"
-                        .'X-Mailer: PHP/' . phpversion();
+                        . 'X-Mailer: PHP/' . phpversion();
 
                     mail("$to", "$subject", "$message", "$headers");
                     unset($_SESSION['prod']);

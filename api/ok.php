4cc9b4fcbc0115678b755fb059fe405b75892e68
segasle <segasle@gmail.com>
2019-07-15 08:00:06 +0300

    подготовка авторизовации через одноклассники

--- api/ok.php
+++ api/ok.php
@@ -5,17 +5,19 @@ $okscope = 'VALUABLE_ACCESS,LONG_ACCESS_TOKEN,PHOTO_CONTENT,GET_EMAIL';
 $okid = '1277595392';
 $secretkey = '47B3C84B19EE3DBD5BF383B7';
 function ok_authorization(){
+
     global $redirect_uri;
     global $okscope;
     global $secretkey;
     global $okid;
-    if (!empty($_GET['code'])){
-        $code = $_GET['code'];
-        $file = json_decode(file_get_contents("https://api.ok.ru/oauth/token.do?code=".$code."&client_id=".$okid."&client_secret=".$secretkey."&redirect_uri=".$redirect_uri."&grant_type=authorization_code"));
+    if (!empty($_POST['code'])){
+        $code = $_POST['code'];
+        $file = file_link("https://api.ok.ru/oauth/token.do?code=".$code."&client_id=".$okid."&client_secret=".$secretkey."&redirect_uri=".$redirect_uri."&grant_type=authorization_code");
+
         $_SESSION['user'] = $file;
         if (isset($_SESSION['user'])){
             $access_token = $_SESSION['user']['access_token'];
-            $users = json_decode(file_get_contents("https://api.ok.ru/oauth/token.do?refresh_token=".$access_token."&client_id=".$okid."&client_secret=".$secretkey."&grant_type=refresh_token"));
+            $users = file_link("https://api.ok.ru/oauth/token.do?refresh_token=".$access_token."&client_id=".$okid."&client_secret=".$secretkey."&grant_type=refresh_token");
 
             echo '<pre>';
             print_r($users);
@@ -25,4 +27,5 @@ function ok_authorization(){
 
 
     }
+    return;
 }

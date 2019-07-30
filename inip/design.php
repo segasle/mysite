5f6fab40e9ce3e2d21905d3a026d244ced3f2c14
segasle <segasle@gmail.com>
2019-07-30 21:35:38 +0300

    карусель

--- inip/design.php
+++ inip/design.php
@@ -7,11 +7,11 @@
             ?>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="fas fa-arrow-left fa-5x" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="fas fa-arrow-right fa-5x" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>

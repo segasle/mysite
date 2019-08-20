122d2f3bc6936b9fa6a8b29fd45124dc7a22cbef
sergejslepenkov <Segasle@gmail.com>
2019-08-20 13:23:53 +0300

    main тег

--- tempate/footer.php
+++ tempate/footer.php
@@ -1,5 +1,5 @@
 </div>
-
+</main>
 <footer>
     <div class="cellar">
         <div class="row">
--- tempate/header.php
+++ tempate/header.php
@@ -57,22 +57,25 @@ if (isset($_SESSION['data'])) {
     <!-- /Yandex.Metrika counter -->
 
     <script>
-        window.fbAsyncInit = function() {
+        window.fbAsyncInit = function () {
             FB.init({
-                appId      : '398843257654116',
-                cookie     : true,
-                xfbml      : true,
-                version    : 'v3.3'
+                appId: '398843257654116',
+                cookie: true,
+                xfbml: true,
+                version: 'v3.3'
             });
 
             FB.AppEvents.logPageView();
 
         };
 
-        (function(d, s, id){
+        (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
             js.src = "https://connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
@@ -271,5 +274,5 @@ if (isset($_SESSION['data'])) {
                 <?php } ?>
             </div>
         </header>
-
-        <div class="contain ndra-container">
+        <main>
+            <div class="contain ndra-container">

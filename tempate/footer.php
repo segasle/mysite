69a345438b7da9bdc32fc19de61a6e84384ad367
sergejslepenkov <Segasle@gmail.com>
2019-09-03 07:02:03 +0300

    обратная связь

--- tempate/footer.php
+++ tempate/footer.php
@@ -28,6 +28,7 @@
 </footer>
 </div>
 </div>
+<script type="text/javascript" src="https://spikmi.com/Widget?Id=1610"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>

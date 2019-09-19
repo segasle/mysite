4cae0fd467188e5be000a0acefa0d36e620e1f6c
sergejslepenkov <Segasle@gmail.com>
2019-09-19 18:41:25 +0300

    выбиралка

--- inip/project.php
+++ inip/project.php
@@ -1,22 +1,6 @@
 <h1>Проекты</h1>
-<form method="post" class="row align-items-center">
-    <div class="col-12 col-sm-12 col-md-8 col-lg-10 col-xl-10">
-        <div class="form-group">
-            <label for="exampleFormControlSelect1">Сортировка</label>
-            <select class="form-control" id="exampleFormControlSelect1" name="sel">
-                <option value="old">Старые</option>
-                <option value="new">Новые</option>
-                <option value="default">По умолчанию</option>
-            </select>
-        </div>
-    </div>
-    <div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-2">
-        <div class="form_grog">
-            <button type="submit" name="submit">Получить</button>
-        </div>
-    </div>
-</form>
 <?php
+include 'tempate/select.php';
 if (isset($_POST['submit'])){
     $data = $_POST;
     if ($data['sel'] == 'new'){
@@ -33,6 +17,4 @@ if (isset($_POST['submit'])){
     }
 }else{
     echo post_project('SELECT * FROM `projects`');
-
 }
-?>
---
+++ tempate/select.php
@@ -0,0 +1,17 @@
+<form method="post" class="row align-items-center">
+    <div class="col-12 col-sm-12 col-md-8 col-lg-10 col-xl-10">
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Сортировка</label>
+            <select class="form-control" id="exampleFormControlSelect1" name="sel">
+                <option value="old">Старые</option>
+                <option value="new">Новые</option>
+                <option value="default">По умолчанию</option>
+            </select>
+        </div>
+    </div>
+    <div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-2">
+        <div class="form_grog">
+            <button type="submit" name="submit">Получить</button>
+        </div>
+    </div>
+</form>

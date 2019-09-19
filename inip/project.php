03c133a08888bf93b1ff6dcab6fcca944a4d3d77
segasle <segasle@gmail.com>
2019-09-19 13:48:21 +0300

    выбиралка

--- inip/project.php
+++ inip/project.php
@@ -3,19 +3,17 @@
     <div class="col-12 col-sm-12 col-md-8 col-lg-10 col-xl-10">
         <div class="form-group">
             <label for="exampleFormControlSelect1"></label>
-            <select class="form-control" id="exampleFormControlSelect1">
-                <option>Старые</option>
-                <option>Новые</option>
+            <select class="form-control" id="exampleFormControlSelect1" name="sel">
+                <option value="old">Старые</option>
+                <option value="new">Новые</option>
             </select>
         </div>
     </div>
-
     <div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-2">
         <div class="form_grog">
             <button type="submit" name="submit">Получить</button>
         </div>
     </div>
-
 </form>
 <?php
 echo post_project();
